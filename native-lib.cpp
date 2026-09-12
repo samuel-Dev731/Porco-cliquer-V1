7d06d706baf585c616f03b171cfe9e59a8ce09dc
samuel-Dev731 <sc2585265@gmail.com>
2026-09-12 13:21:11 -0300

    Fix JNI function signatures in native-lib.cpp

--- native-lib.cpp
+++ native-lib.cpp
@@ -3,13 +3,13 @@
 extern "C" {
 
 JNIEXPORT jlong JNICALL
-Java_com_porcocliquer_MainActivity_calcularCustoMultiplicadorNativo(JNIEXPORT jenv, jobject thiz, jlong nivelMultiplicador) {
+Java_com_porcocliquer_MainActivity_calcularCustoMultiplicadorNativo(JNIEnv *jenv, jobject thiz, jlong nivelMultiplicador) {
     jlong custoBase = 50;
     return custoBase * nivelMultiplicador;
 }
 
 JNIEXPORT jlong JNICALL
-Java_com_porcocliquer_MainActivity_calcularCustoFazendaNativo(JNIEXPORT jenv, jobject thiz, jlong nivelFazenda) {
+Java_com_porcocliquer_MainActivity_calcularCustoFazendaNativo(JNIEnv *jenv, jobject thiz, jlong nivelFazenda) {
     jlong custoBase = 100;
     return custoBase * (nivelFazenda + 1);
 }
